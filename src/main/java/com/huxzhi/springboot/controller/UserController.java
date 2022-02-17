99220ab3ada294a0e01cb7be34c2427a44b1f359
Huxzhi <huxzhi@gmail.com>
2022-02-17 21:45:48 +0800

    集成Swagger3 和 mybatis-plus进行sql语句测试

--- src/main/java/com/huxzhi/springboot/controller/UserController.java
+++ src/main/java/com/huxzhi/springboot/controller/UserController.java
@@ -1,21 +1,21 @@
 package com.huxzhi.springboot.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.huxzhi.springboot.bean.User;
-import com.huxzhi.springboot.mapper.UserMapper;
 import com.huxzhi.springboot.service.Ipml.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RequestMapping("/user")
 @RestController // 等价于@ResponseBody + @Controller。
 public class UserController {
 
-    @Autowired
-    private UserMapper userMapper;
+//    @Autowired
+//    private UserMapper userMapper;
 
     @Autowired
     private UserServiceImpl userService;
@@ -23,7 +23,7 @@ public class UserController {
     //查询所有数据
     @GetMapping
     public List<User> findAllUsers() {
-        return userMapper.findAll();
+        return userService.list();
     }
 
     @PostMapping
@@ -33,30 +33,56 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
-    public Integer deleteUser(@PathVariable Integer id) {
-        return userMapper.deleteUserById(id);
+    public boolean deleteUser(@PathVariable Integer id) {
+        return userService.removeById(id);
     }
 
 
     //分页查询
     //接口路径：/user/page
     //@RequestParam 接受 ?pageNum=1&pageSize=10
+//    @GetMapping("/page")
+//    public Map<String, Object> findPageUsers(@RequestParam Integer pageNum,
+//                                             @RequestParam Integer pageSize,
+//                                             @RequestParam String username) {
+//        pageNum = (pageNum - 1) * pageSize;
+//
+//        List<User> dataUser = userMapper.selectPageUsers(pageNum, pageSize, username);
+//        Integer totalUser = userMapper.selectTotalUser(username);
+//
+//        Map<String, Object> res = new HashMap<>();
+//        res.put("data", dataUser);
+//        res.put("total", totalUser);
+//
+//        return res;
+//    }
+
+    //分页查询 - mybatis-plus的方式
     @GetMapping("/page")
-    public Map<String, Object> findPageUsers(@RequestParam Integer pageNum,
-                                             @RequestParam Integer pageSize,
-                                             @RequestParam String username) {
-        pageNum = (pageNum - 1) * pageSize;
+    public IPage<User> findPageUsers(@RequestParam Integer pageNum,
+                                     @RequestParam Integer pageSize,
+                                     @RequestParam(defaultValue = "") String username,
+                                     @RequestParam(defaultValue = "") String nickname,
+                                     @RequestParam(defaultValue = "") String address) {
+        IPage<User> page = new Page<>(pageNum, pageSize);
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
 
-        List<User> dataUser = userMapper.selectPageUsers(pageNum, pageSize, username);
-        Integer totalUser = userMapper.selectTotalUser(username);
+        // queryWrapper.or().like("address", address); //错误的写法，会导致前面的条件全部失效
 
-        Map<String, Object> res = new HashMap<>();
-        res.put("data", dataUser);
-        res.put("total", totalUser);
+        if (!"".equals(username)) {
+            queryWrapper.like("username", username);
+        }
+        if (!"".equals(nickname)) {
+            queryWrapper.like("nickname", nickname);
+        }
+        if (!"".equals(address)) {
+            queryWrapper.like("address", address);
+        }
 
-        return res;
+
+        return userService.page(page, queryWrapper);
+
     }
-
 
 //    @PutMapping
 //    public String putUser() {
--- src/main/java/com/huxzhi/springboot/mapper/UserMapper.java
+++ src/main/java/com/huxzhi/springboot/mapper/UserMapper.java
@@ -2,33 +2,27 @@ package com.huxzhi.springboot.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.huxzhi.springboot.bean.User;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
 
 //@Mapper
 public interface UserMapper extends BaseMapper<User> {
 
-    @Select("select * from sys_user ")
-    List<User> findAll();
-
-    @Insert("INSERT INTO sys_user(username,password,nickname,email,phone,address) VALUES" +
-            " (#{username},#{password},#{nickname},#{email},#{phone},#{address});")
-    int insert(User user);
-
-    //在配置文件定义了动态sql
-    int update(User user);
-
-    @Delete("delete from sys_user where id =#{id}")
-    Integer deleteUserById(@Param("id") Integer id);
-
-    //分页查询
-    @Select("SELECT * FROM sys_user where username like concat('%',#{username},'%') LIMIT #{pageNum},#{pageSize};")
-    List<User> selectPageUsers(Integer pageNum, Integer pageSize, String username);
-
-    @Select("select count(*) from sys_user where username like concat('%',#{username},'%') ")
-    Integer selectTotalUser(String username);
+//    @Select("select * from sys_user ")
+//    List<User> findAll();
+//
+//    @Insert("INSERT INTO sys_user(username,password,nickname,email,phone,address) VALUES" +
+//            " (#{username},#{password},#{nickname},#{email},#{phone},#{address});")
+//    int insert(User user);
+//
+//    //在配置文件定义了动态sql
+//    int update(User user);
+//
+//    @Delete("delete from sys_user where id =#{id}")
+//    Integer deleteUserById(@Param("id") Integer id);
+//
+//    //分页查询
+//    @Select("SELECT * FROM sys_user where username like concat('%',#{username},'%') LIMIT #{pageNum},#{pageSize};")
+//    List<User> selectPageUsers(Integer pageNum, Integer pageSize, String username);
+//
+//    @Select("select count(*) from sys_user where username like concat('%',#{username},'%') ")
+//    Integer selectTotalUser(String username);
 }
