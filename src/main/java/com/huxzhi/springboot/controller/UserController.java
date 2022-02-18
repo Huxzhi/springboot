4f97c1bd6be4146f138a7ed3513dc2b16e2ff2ca
Huxzhi <huxzhi@gmail.com>
2022-02-18 14:37:38 +0800

    成功生成代码
    还会进行定制化修改
    
    fix 遇到的坑
    
    fix 完善代码生成器设置

--- src/main/java/com/huxzhi/springboot/controller/UserController.java
+++ src/main/java/com/huxzhi/springboot/controller/UserController.java
@@ -1,21 +1,80 @@
 package com.huxzhi.springboot.controller;
 
+//导入需要的包
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.huxzhi.springboot.entity.User;
+import com.huxzhi.springboot.service.IUserService;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.stereotype.Controller;
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author huxzhi
  * @since 2022-02-18
  */
-@Controller
-@RequestMapping("//user")
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
+    @Resource
+    private IUserService userService;
+
+    //新增或者更新
+    @PostMapping
+    public boolean save(@RequestBody User user) {
+        return userService.saveOrUpdate(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable Integer id) {
+        return userService.removeById(id);
+    }
+
+    @PostMapping("/del/batch")
+    public boolean deleteBatch(@RequestBody List<Integer> ids) {
+        return userService.removeByIds(ids);
+    }
+
+    //查询所有数据
+    @GetMapping
+    public List<User> findAll() {
+        return userService.list();
+    }
+
+    @GetMapping("/{id}")
+    public User findOne(@PathVariable Integer id) {
+        return userService.getById(id);
+    }
+
+    @GetMapping("/page")
+    public Page<User> findPage(@RequestParam Integer pageNum,
+                               @RequestParam Integer pageSize,
+                               @RequestParam(defaultValue = "") String username,
+                               @RequestParam(defaultValue = "") String email,
+                               @RequestParam(defaultValue = "") String address) {
+
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        if (!"".equals(username)) {
+            queryWrapper.like("username", username);
+        }
+        if (!"".equals(email)) {
+            queryWrapper.like("email", email);
+        }
+        if (!"".equals(address)) {
+            queryWrapper.like("address", address);
+        }
+
+        //进行倒序输出
+        queryWrapper.orderByDesc("id");
+
+        return userService.page(new Page<>(pageNum, pageSize), queryWrapper);
+    }
 }
 
