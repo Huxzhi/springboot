cbcb536de02f4bc1eb5b27d6dd60417871905347
Huxzhi <huxzhi@gmail.com>
2022-02-23 23:07:31 +0800

    修改主页背景

---
+++ src/main/java/com/huxzhi/springboot/common/RoleEnum.java
@@ -0,0 +1,8 @@
+package com.huxzhi.springboot.common;
+
+
+public enum RoleEnum {
+    ROLE_ADMIN, ROLE_USER;
+}
+
+
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
-  <div style="color: #666;font-size: 14px;">
-    <div style="padding-bottom: 20px">
-      <b>欢迎你！{{ user.nickname }}</b>
+  <div class="bg" style="height: 100%;width: 100%">
+    <div style="color: #666;font-size: 14px;padding-top: 30px ;">
+
+      <el-card style=" margin-left: 20px;padding: 10px;width: 600px">
+        <h1> 欢迎你！{{ user.nickname }}</h1>
+        <el-divider/>
+        祝您 虎虎生威，虎年大吉！
+      </el-card>
+      <div style="height: 1px; background: #ddd; margin: 20px 0;width: 620px"/>
     </div>
-    <el-card>
-      青哥出手，马上拥有
-      <el-divider/>
-      虎虎生威，虎年大吉
-    </el-card>
-    <div style="height: 1px; background: #ddd; margin: 20px 0"/>
 
 
   </div>
@@ -27,3 +27,13 @@ export default {
 
 }
 </script>
+<style>
+.bg {
+
+  background-image: url(../assets/homebg.png);
+  -moz-background-size: 100% 100%;
+  -webkit-background-size: 100% 100%;
+
+}
+
+</style>
