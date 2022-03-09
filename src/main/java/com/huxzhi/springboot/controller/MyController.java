eef31ab304dadd09d9d08454f0db529b26452747
Huxzhi <huxzhi@gmail.com>
2022-03-09 20:41:09 +0800

    Merge pull request #4 from Huxzhi/feature-postResidence
    feature: 自助申请暂住证

--- src/main/java/com/huxzhi/springboot/controller/MyController.java
+++ src/main/java/com/huxzhi/springboot/controller/MyController.java
@@ -1,11 +1,9 @@
 package com.huxzhi.springboot.controller;
 
 import com.huxzhi.springboot.common.Result;
+import com.huxzhi.springboot.controller.dto.PostResidenceDTO;
 import com.huxzhi.springboot.service.MyService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -18,6 +16,12 @@ public class MyController {
     @GetMapping
     public Result getData(@RequestParam Integer id) {
         return Result.success(myService.getData(id));
+    }
+
+    @PostMapping("postresidence")
+    public Result postResidence(@RequestBody PostResidenceDTO postResidenceDTO) {
+        myService.postResidence(postResidenceDTO);
+        return Result.success();
     }
 
 }
--- src/main/java/com/huxzhi/springboot/entity/Residence.java
+++ src/main/java/com/huxzhi/springboot/entity/Residence.java
@@ -1,5 +1,7 @@
 package com.huxzhi.springboot.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -26,6 +28,7 @@ public class Residence implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @ApiModelProperty("id")
+    @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
     @ApiModelProperty("姓名")
