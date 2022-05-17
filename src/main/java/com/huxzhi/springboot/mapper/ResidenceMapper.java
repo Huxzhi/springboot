f31e758f2ea75406aa9f32811df69dafbdcadb7f
Huxzhi <huxzhi@gmail.com>
2022-05-17 16:41:55 +0800

    显示暂住证时效和查看该用户更多暂住证记录

--- src/main/java/com/huxzhi/springboot/mapper/ResidenceMapper.java
+++ src/main/java/com/huxzhi/springboot/mapper/ResidenceMapper.java
@@ -1,12 +1,16 @@
 package com.huxzhi.springboot.mapper;
 
-import com.huxzhi.springboot.entity.Residence;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.huxzhi.springboot.entity.Residence;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * <p>
- *  Mapper 接口
+ * Mapper 接口
  * </p>
  *
  * @author huxzhi
@@ -14,5 +18,11 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface ResidenceMapper extends BaseMapper<Residence> {
+    /**
+     * 根据暂住证记录反查用户id
+     */
+    @Select("SELECT id_user id FROM fp_user_residence WHERE id_residence = #{id_residence}")
+    List<Integer> getUserIdByResidence(@Param("id_residence") Integer id_residence);
+
 
 }
